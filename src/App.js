15a7c19b3e7a2ff5e77e88c1ee4460538f4c2f53
compile-run-Ali <91612802+compile-run-Ali@users.noreply.github.com>
2022-01-30 15:11:37 +0500

    added you won when conditions met

--- src/App.js
+++ src/App.js
@@ -1,48 +1,82 @@
 import Die from "./components/die";
 import React from "react";
-import {nanoid} from "nanoid";
+import { nanoid } from "nanoid";
 import "./App.css";
 
 function App() {
-  const [tenzies,setTenzies] = React.useState(false);
+  const [tenzies, setTenzies] = React.useState(false);
   const [dice, setDice] = React.useState(allNewDice());
 
   React.useEffect(() => {
-    console.log("change")
+    let equal = true;
+    let held = true;
+    for (let i = 0; i < dice.length; i++) {
+      if (dice[i].value !== dice[0].value) equal = false;
+      if (!dice[i].isHeld) held = false;
+    }
+    if (equal && held) {
+      setTenzies(true);
+      console.log("you won!");
+    }
   }, [dice]);
 
   function allNewDice() {
     const dieArray = [];
     for (let i = 0; i < 10; i++) {
-      dieArray[i] = {id:nanoid(),value:Math.floor(Math.random() * 6) + 1,isHeld:false};
+      dieArray[i] = {
+        id: nanoid(),
+        value: Math.floor(Math.random() * 6) + 1,
+        isHeld: false,
+      };
     }
     return dieArray;
   }
 
   const die = dice.map((die) => {
-    return <Die key={die.id} id={die.id} value={die.value} isheld={die.isHeld} hold={holdDice}/>;
+    return (
+      <Die
+        key={die.id}
+        id={die.id}
+        value={die.value}
+        isheld={die.isHeld}
+        hold={holdDice}
+      />
+    );
   });
 
   function handleClick() {
-    setDice(oldDice=>oldDice.map(die=>{
-      return die.isHeld?die:{id:nanoid(),value:Math.floor(Math.random() * 6) + 1,isHeld:false}
-    }));
+    setDice((oldDice) =>
+      oldDice.map((die) => {
+        return die.isHeld
+          ? die
+          : {
+              id: nanoid(),
+              value: Math.floor(Math.random() * 6) + 1,
+              isHeld: false,
+            };
+      })
+    );
   }
 
   function holdDice(id) {
-    setDice(oldDice => oldDice.map(die => {
-        return die.id === id ? 
-            {...die, isHeld: !die.isHeld} : die
-            
-    }))
-}
+    setDice((oldDice) =>
+      oldDice.map((die) => {
+        return die.id === id ? { ...die, isHeld: !die.isHeld } : die;
+      })
+    );
+  }
 
   return (
     <main>
       <h1>Tenzies</h1>
-      <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
+      <p className="instructions">
+        Roll until all dice are the same. Click each die to freeze it at its
+        current value between rolls.
+      </p>
       <div className="die-container">{die}</div>
-      <button className="roll" onClick={handleClick}>Roll</button>
+      <button className="roll" onClick={handleClick}>
+        Roll
+      </button>
     </main>
   );
 }
